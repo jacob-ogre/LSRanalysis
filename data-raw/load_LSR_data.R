8368cb8880df1c36fff0990e0ca1f695736f0d9d
Jacob Malcom <jmalcom@defenders.org>
2017-10-04 12:23:04 -0400

    Update for 2017 data.

--- data-raw/load_LSR_data.R
+++ data-raw/load_LSR_data.R
@@ -2,7 +2,8 @@
 
 library(readxl)
 
-LSR_monitor <- read_excel("data-raw/LSR_2007_2010-2016.xlsx")
+# LSR_monitor <- read_excel("data-raw/LSR_2007_2010-2016.xlsx")
+LSR_monitor <- read_excel("data-raw/LSR_2007_2010-2017.xlsx")
 
 names(LSR_monitor)[6] <- "Area"
 devtools::use_data(LSR_monitor, overwrite = TRUE)
